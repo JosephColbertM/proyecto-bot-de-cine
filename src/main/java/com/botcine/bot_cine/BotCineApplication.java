eaf91709af58fcf470e5d9e757f82624ebf09e10
Joseph Colbert <joseph97cm@gmail.com>
2022-04-11 11:18:34 -0400

    Proyecto Bot de Cine

---
+++ src/main/java/com/botcine/bot_cine/BotCineApplication.java
@@ -0,0 +1,13 @@
+package com.botcine.bot_cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botcine/bot_cine/BotCineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botcine.bot_cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
