5868a91c3eda9f1bdac5f1e8d8c24e9bce347335
Joseph Colbert <joseph97cm@gmail.com>
2022-04-14 14:12:00 -0400

    Implementacion de codigo reformulado

--- src/main/java/com/botcine/bot_cine/BotCineApplication.java
+++ src/main/java/com/botcine/bot_cine/BotCineApplication.java
@@ -1,13 +1,27 @@
 package com.botcine.bot_cine;
 
+
+import com.botcine.bot_cine.chat.CineLongPollingBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 
 @SpringBootApplication
 public class BotCineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotCineApplication.class, args);
+		try {
+			//Inicializamos libreria de bots
+			TelegramBotsApi telegramApi = new TelegramBotsApi(DefaultBotSession.class);
+			//Registramos la implementacion de nuestro BOT
+			telegramApi.registerBot(new CineLongPollingBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
