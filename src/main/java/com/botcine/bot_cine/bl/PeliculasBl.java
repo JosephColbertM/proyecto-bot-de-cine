695044b5dd24c05c9ba8531b838c67a4f494d5dd
Marcelo Miranda <cmgm274@gmail.com>
2022-04-21 20:07:50 -0400

    Prueba de nombre

--- src/main/java/com/botcine/bot_cine/bl/PeliculasBl.java
+++ src/main/java/com/botcine/bot_cine/bl/PeliculasBl.java
@@ -10,7 +10,7 @@ public class PeliculasBl {
     }
     public List<PeliculasDto> findLast10PermissionsByChatId(Long chatId) {
         List<PeliculasDto> result = new ArrayList<>();
-        result.add(new PeliculasDto("The Batman", "2:50", "Thriller","10:00,12:00,14:10,15:30,17:30"));
+        result.add(new PeliculasDto("Batman", "2:50", "Thriller","10:00,12:00,14:10,15:30,17:30"));
         result.add(new PeliculasDto("Sonic 2", "1:42", "Aventura","11:20,11:40,14:50,15:00,18:50"));
         result.add(new PeliculasDto("IT", "2:10", "Terror","10:00,12:00,14:10,15:30,17:30"));
         result.add(new PeliculasDto("Animales Fantasticos", "2:30", "Aventura","11:20,11:40,14:50,15:00,18:50"));
