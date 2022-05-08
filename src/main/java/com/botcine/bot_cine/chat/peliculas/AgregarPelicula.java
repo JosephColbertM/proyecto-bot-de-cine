ecd37959ca77f4d7c563e8c58cb29dcc8628328d
Marcelo Miranda <cmgm274@gmail.com>
2022-05-08 13:25:37 -0400

    Mostrado de peliculas

--- src/main/java/com/botcine/bot_cine/chat/peliculas/AgregarPelicula.java
+++ src/main/java/com/botcine/bot_cine/chat/peliculas/AgregarPelicula.java
@@ -8,7 +8,7 @@ import com.botcine.bot_cine.dto.PeliculasDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -44,23 +44,22 @@ public class AgregarPelicula extends AbstractProcess {
             sb.append("Genero: \r\n");
             sendStringBuffer(bot, chatId, sb);
             setStatus("AWAITING_USER_RESPONSE");
-            System.out.println("Inicio");
         } else if (this.getStatus().equals("AWAITING_USER_RESPONSE")) {
             Message message = update.getMessage();
             if (message.hasText()) {
                 // Intentamos transformar en número
                 String text = message.getText(); // El texto contiene asdasdas
                 try {
-                    System.out.println("Entrada");
+
                     String c[] = text.split("/");
                     peliculasBl.savePeliculas(c[0], c[1], c[2]);
                     setStatus("STARTED");
                     return new AccesoPeliculas();
                 } catch (Exception ex) {
-                    System.out.println(ex);
+
                 }
             } else { // Si me enviaron algo diferente de un texto.
-                System.out.println("Fallo");
+
                 sb.append("PARA AGREGAR UNA PELÍCULA, DEBERA INGRESAR LOS DATOS EN EL SIGUIENTE ORDEN:\r\n\n");
                 sb.append("Nombre: \r\n");
                 sb.append("Duración: \r\n");
--- src/main/java/com/botcine/bot_cine/chat/peliculas/ListaPeliculas.java
+++ src/main/java/com/botcine/bot_cine/chat/peliculas/ListaPeliculas.java
@@ -1,14 +1,18 @@
 package com.botcine.bot_cine.chat.peliculas;
 
 import com.botcine.bot_cine.bl.PeliculasBl;
-import com.botcine.bot_cine.chat.*;
+import com.botcine.bot_cine.chat.AbstractProcess;
+import com.botcine.bot_cine.chat.AccesoPeliculas;
+import com.botcine.bot_cine.chat.CineLongPollingBot;
 import com.botcine.bot_cine.dto.PeliculasDto;
-import org.jvnet.hk2.annotations.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
+
 @Service
 public class ListaPeliculas extends AbstractProcess {
 
@@ -17,7 +21,7 @@ public class ListaPeliculas extends AbstractProcess {
     @Autowired
     public ListaPeliculas(PeliculasBl peliculasBl) {
         this.peliculasBl = peliculasBl;
-        this.setName("Lista de las peliculas");
+        this.setName("Agregar película");
         this.setDefault(false);
         this.setExpires(false);
         this.setStartDate(System.currentTimeMillis()/1000);
@@ -28,16 +32,28 @@ public class ListaPeliculas extends AbstractProcess {
 
     @Override
     public AbstractProcess handle(ApplicationContext context, Update update, CineLongPollingBot bot) {
+
         Long chatId = update.getMessage().getChatId();
-        List<PeliculasDto> peliculaList = peliculasBl.findLast10PermissionsByChatId(chatId);
+        List<PeliculasDto> peliculaList = peliculasBl.findLast10PermissionsByChatId(chatId);//cambiar
         StringBuffer sb = new StringBuffer();
-        sb.append("PELÍCULAS EN CARTELERA\n\r\n");
+        sb.append("Lista de peliculas:\r\n\n");
+
+        System.out.println(peliculaList.size());
         for(PeliculasDto pelicula: peliculaList) {
             sb.append(pelicula.toString()).append("\n\r");
         }
-        sb.append("Ingrese cualquier caracter para volver al menu de películas");
-        sendStringBuffer(bot, chatId, sb);
-        return new AccesoPeliculas();
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId.toString());
+        sendMessage.setText(sb.toString());
+
+        try {
+            bot.execute(sendMessage);
+        } catch (Exception ex) {
+            // relanzamos la excepción
+            throw new RuntimeException(ex);
+        }
+        return context.getBean(AccesoPeliculas.class);
     }
 
     @Override
@@ -54,4 +70,6 @@ public class ListaPeliculas extends AbstractProcess {
     public AbstractProcess onTimeout() {
         return null;
     }
+
+
 }
